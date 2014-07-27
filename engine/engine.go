42aebe602005273bc54e6a07a4ef5b8437a43bcc
Don Garrett <dgarrett@acm.org>
2014-07-26 18:34:56 -0700

    Enable Engine support for Properties.

--- engine/engine.go
+++ engine/engine.go
@@ -2,35 +2,46 @@ package engine
 
 import (
 	"github.com/DonGar/go-house/engine/actions"
+	"github.com/DonGar/go-house/engine/properties"
 	"github.com/DonGar/go-house/engine/rules"
 	"github.com/DonGar/go-house/status"
 	"strings"
 )
 
 const rules_watch_url = "status://*/rule/*"
+const properties_watch_url = "status://*/property/*"
 
 type Engine struct {
-	status  *status.Status
-	actions actions.ActionRegistrar
-	rules   *watcher
+	status     *status.Status
+	actions    actions.ActionRegistrar
+	rules      *watcher
+	properties *watcher
 }
 
 func NewEngine(status *status.Status) (engine *Engine, e error) {
-	engine = &Engine{status, actions.StandardActions(), nil}
+	engine = &Engine{status, actions.StandardActions(), nil, nil}
 	engine.rules = newWatcher(status, rules_watch_url, engine.newRule)
+	engine.properties = newWatcher(status, properties_watch_url, engine.newProperty)
 
 	return engine, nil
 }
 
 func (e *Engine) Stop() {
 	e.rules.Stop()
+	e.properties.Stop()
 }
 
-func (e *Engine) newRule(url string, body *status.Status) (stoppable, error) {
+func nameFromUrl(url string) string {
 	// Find it's name.
 	// status://adapter_name/rules/<name>/
 	url_parts := strings.Split(url, "/")
-	name := url_parts[len(url_parts)-1]
+	return url_parts[len(url_parts)-1]
+}
 
-	return rules.NewRule(e.status, e.actions, name, body)
+func (e *Engine) newRule(url string, body *status.Status) (stoppable, error) {
+	return rules.NewRule(e.status, e.actions, nameFromUrl(url), body)
+}
+
+func (e *Engine) newProperty(url string, body *status.Status) (stoppable, error) {
+	return properties.NewProperty(e.status, nameFromUrl(url), body)
 }
