5a5ae2905ef66a65ac5de7b5550c8172f45bacc2
Don Garrett <dgarrett@acm.org>
2014-10-19 19:14:18 -0700

    Stoppable: Add better comments.
    Add comments that describe how to use this class. This still
    doesn't work with generated documentation, but will do until
    I figure out how that works.

--- stoppable/stoppable.go
+++ stoppable/stoppable.go
@@ -5,20 +5,24 @@ type Stoppable interface {
 	Stop()
 }
 
+// Compose this type.
 type Base struct {
 	StopChan chan bool
 }
 
+// Use this helper to construct the composed value.
 func NewBase() Base {
 	return Base{make(chan bool)}
 }
 
+// Override only if you need additional work here.
 func (b *Base) Stop() {
 	b.StopChan <- true
 	<-b.StopChan
 }
 
-// This is a default (and uninteresting) background process.
+// This is a default (and uninteresting) background process. Override as needed.
+// call go x.Handler() in your constructor.
 func (b *Base) Handler() {
 	for {
 		select {
